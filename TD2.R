f2b7556fcb058738a38b896e930981ef661074ef
julienportal66 <julien.portal66@gmail.com>
2026-02-04 16:48:41 +0100

    Update TD2.R
    Fin du TD2

--- TD2.R
+++ TD2.R
@@ -68,7 +68,7 @@ corrplot(matriceCor, method="color",
 #Body_Kart
 matriceCor = round(cor(bodies_karts[ , - 1]),1)
 corrplot(matriceCor, method="color",  
-         type="upper", order="hclust", 
+         type="upper", order="hclust", #upper (partie haute), lower partie basse
          addCoef.col = "black", # Ajout du coefficient de corrélation
          tl.col="black", tl.srt=45, #Rotation des étiquettes de textes
          # Cacher les coefficients de corrélation sur la diagonale
@@ -87,3 +87,63 @@ corrplot(matriceCor, method="color",
 #Attention on ne peut pas le trouvers car ecart type nul
 
 #Exercice 3
+#Créer un object resultat avec uniquement le nom du Driver et son Weight.
+resultat = drivers [,c("Driver", "Weight")]
+View(resultat)
+
+#Créer un object resultat avec uniquement le nom du Driver et son Acceleration sur les 10 premières lignes.
+resultat = drivers [1:10 ,c("Driver","Acceleration")]
+View(resultat)
+
+#Créer un object resultat sans les colonnes 5, 7 et 9.
+resultat = drivers [,c(-5, -7, -9)]
+View(resultat)
+
+#Créer un object resultatsans les colonnes Weight et Acceleration.
+resultat = drivers[ , -c("Weight","Acceleration")] #marche pas car uniquement pour index numérique (pas les textes)
+resultat = drivers[ , -c(2,3)] # <- La solution
+
+#Créer un object resultat avec uniquement les colonnes Driver, Acceleration et Weight dans cet ordre.
+resultat = drivers [,c("Driver","Acceleration","Weight")]
+View(resultat) # Colonnes conservé par l'odre que l'on définit
+
+#Créer un object resultat avec uniquement les Driver 3 , 12 et 32 dans cet ordre.
+resultat = drivers [c(3,12,32), "Driver" ]
+View(resultat) 
+
+#Créer un object resultat avec uniquement les Driver 32 , 3 , 12 dans cet ordre.
+resultat = drivers [c(32,3,12), "Driver"]
+View(resultat) #Ordre des lignes défini conservé
+
+#Créer un object resultat avec uniquement les colonnes Driver et Weight en triant les conducteurs du plus léger au plus lourd avec la fonction order().
+rang = order(drivers$Weight) #Tri fait sur le weight
+resultat = drivers[ rang  , c("Driver", "Weight") ]
+View(resultat)                   
+
+#Créer un object resultat avec uniquement les colonnes Driver et Acceleration en triant les conducteurs du plus rapide au moins rapide.
+rang = order(drivers$Acceleration, decreasing = TRUE)# True pour mettre en Decroissant
+resultat = drivers [rang , c("Driver","Acceleration")]
+View(resultat)
+
+#Créer un object resultat avec les colonnes Driver, Weight et Acceleration en triant les conducteurs du plus rapide au moins rapide puis du plus léger au plus lourd.
+rang = order(drivers$Acceleration, drivers$Weight, decreasing = c(TRUE,FALSE))#True pour "acceleration" et False pour "Weight"
+resultat = drivers [rang, c("Driver", "Acceleration","Weight")]
+View(resultat)
+
+#Exercice 4 - GOAT
+#Créer un object topDriver avec les colonnes Driver et Acceleration avec le ou les conducteurs avec la plus grande Acceleration.
+help(subset)              #On choisi les meilleures acceleration 
+topDriver = subset(x = drivers,
+                   subset = Acceleration == max(Acceleration), 
+                   select = c("Driver","Acceleration"))
+View(topDriver)
+
+#Créer un object topGlider, topTires et topBody avec la même logique de conserver uniquement les meilleurs statistiques d'Acceleration.
+topGlider = subset(x = gliders, subset = Acceleration == max(Acceleration), select = c("Glider", "Acceleration"))
+View(topGlider)
+
+topTires = subset(x = tires, subset = Acceleration == max(Acceleration), select = c("Tire","Acceleration"))
+View(topTires)
+
+topBody = subset(x = bodies_karts, subset = Acceleration ==max(Acceleration) , select = c("Body","Acceleration"))
+View(topBody)
